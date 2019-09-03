7c65361d1e6af2e0541c7c9bce6e50082bf8e400
xKang <xkangxyz@gmail.com>
2019-09-03 22:07:47 +1200

    2019年9月3日22:06:58 add android app, readme and some change on hardware

---
+++ Readme.md
@@ -0,0 +1,3 @@
+Hardware are Unlicense
+android App are fork and modify from  [SimpleBluetoothTerminal](#https://github.com/kai-morichSimpleBluetoothTerminal) base on MIT license
+many thanks
---
+++ android App/app/src/main/java/de/kai_morich/simple_bluetooth_terminal/TerminalFragment.java
@@ -0,0 +1,327 @@
+package de.kai_morich.simple_bluetooth_terminal;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.hardware.SensorEventListener;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.method.ScrollingMovementMethod;
+import android.text.style.ForegroundColorSpan;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.support.v4.content.ContextCompat.getSystemService;
+
+public class TerminalFragment extends Fragment implements ServiceConnection, SerialListener, SensorEventListener {
+
+    private enum Connected { False, Pending, True }
+
+    private String deviceAddress;
+    private String newline = "\r\n";
+
+    private TextView receiveText;
+
+    private SerialSocket socket;
+    private SerialService service;
+    private boolean initialStart = true;
+    private Connected connected = Connected.False;
+
+    /*
+     * I added it
+     */
+    private static final float gravity  = 9.81f;
+    private SensorManager mSensorManager;
+    private Sensor mySensor;
+
+    public TerminalFragment() {
+    }
+
+    /*
+     * Lifecycle
+     */
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+        setRetainInstance(true);
+        deviceAddress = getArguments().getString("device");
+    }
+
+    @Override
+    public void onDestroy() {
+        if (connected != Connected.False)
+            disconnect();
+        getActivity().stopService(new Intent(getActivity(), SerialService.class));
+        super.onDestroy();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if(service != null)
+            service.attach(this);
+        else
+            getActivity().startService(new Intent(getActivity(), SerialService.class)); // prevents service destroy on unbind from recreated activity caused by orientation change
+    }
+
+    @Override
+    public void onStop() {
+        if(service != null && !getActivity().isChangingConfigurations())
+            service.detach();
+        super.onStop();
+    }
+
+    @SuppressWarnings("deprecation") // onAttach(context) was added with API 23. onAttach(activity) works for all API versions
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        getActivity().bindService(new Intent(getActivity(), SerialService.class), this, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    public void onDetach() {
+        try { getActivity().unbindService(this); } catch(Exception ignored) {}
+        super.onDetach();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(initialStart && service !=null) {
+            initialStart = false;
+            getActivity().runOnUiThread(this::connect);
+        }
+        if(connected == Connected.True){
+            SensorInit();
+        }
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener((SensorEventListener) this);
+    }
+
+
+    @Override
+    public void onServiceConnected(ComponentName name, IBinder binder) {
+        service = ((SerialService.SerialBinder) binder).getService();
+        if(initialStart && isResumed()) {
+            initialStart = false;
+            getActivity().runOnUiThread(this::connect);
+        }
+    }
+
+    @Override
+    public void onServiceDisconnected(ComponentName name) {
+        service = null;
+    }
+
+    /*
+     * UI
+     */
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_terminal, container, false);
+        receiveText = view.findViewById(R.id.receive_text);                          // TextView performance decreases with number of spans
+        receiveText.setTextColor(getResources().getColor(R.color.colorRecieveText)); // set as default color to reduce number of spans
+        receiveText.setMovementMethod(ScrollingMovementMethod.getInstance());
+        TextView sendText = view.findViewById(R.id.send_text);
+        View sendBtn = view.findViewById(R.id.send_btn);
+        sendBtn.setOnClickListener(v -> send(sendText.getText().toString()));
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_terminal, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.clear) {
+            receiveText.setText("");
+            return true;
+        } else if (id ==R.id.newline) {
+            String[] newlineNames = getResources().getStringArray(R.array.newline_names);
+            String[] newlineValues = getResources().getStringArray(R.array.newline_values);
+            int pos = java.util.Arrays.asList(newlineValues).indexOf(newline);
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle("Newline");
+            builder.setSingleChoiceItems(newlineNames, pos, (dialog, item1) -> {
+                newline = newlineValues[item1];
+                dialog.dismiss();
+            });
+            builder.create().show();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /*
+     * Serial + UI
+     */
+    private void connect() {
+        try {
+            BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            BluetoothDevice device = bluetoothAdapter.getRemoteDevice(deviceAddress);
+            String deviceName = device.getName() != null ? device.getName() : device.getAddress();
+            status("connecting...");
+            connected = Connected.Pending;
+            socket = new SerialSocket();
+            service.connect(this, "Connected to " + deviceName);
+            socket.connect(getContext(), service, device);
+        } catch (Exception e) {
+            onSerialConnectError(e);
+        }
+    }
+
+    private void disconnect() {
+        connected = Connected.False;
+        service.disconnect();
+        socket.disconnect();
+        socket = null;
+    }
+
+    private void send(String str) {
+        if(connected != Connected.True) {
+            Toast.makeText(getActivity(), "not connected", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        try {
+            SpannableStringBuilder spn = new SpannableStringBuilder(str+'\n');
+            spn.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorSendText)), 0, spn.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            receiveText.append(spn);
+            byte[] data = (str + newline).getBytes();
+            socket.write(data);
+        } catch (Exception e) {
+            onSerialIoError(e);
+        }
+    }
+
+    private void receive(byte[] data) {
+        receiveText.append(new String(data));
+    }
+
+    private void status(String str) {
+        SpannableStringBuilder spn = new SpannableStringBuilder(str+'\n');
+        spn.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.colorStatusText)), 0, spn.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        receiveText.append(spn);
+    }
+
+    /*
+     * SerialListener
+     */
+    @Override
+    public void onSerialConnect() {
+        status("connected");
+        connected = Connected.True;
+        SensorInit();
+    }
+
+    @Override
+    public void onSerialConnectError(Exception e) {
+        status("connection failed: " + e.getMessage());
+        disconnect();
+    }
+
+    @Override
+    public void onSerialRead(byte[] data) {
+        receive(data);
+    }
+
+    @Override
+    public void onSerialIoError(Exception e) {
+        status("connection lost: " + e.getMessage());
+        disconnect();
+    }
+
+    /*
+    * I added it
+    */
+    protected void SensorInit(){
+        mSensorManager = (SensorManager)getActivity().getSystemService(Context.SENSOR_SERVICE);
+        mySensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+        mSensorManager.registerListener((SensorEventListener) this, mySensor,1000000);
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        int throttle = cal_throttle(event.values[0],event.values[1]);
+        int steering = cal_steering(event.values[2],event.values[1]);
+        java.lang.String sending =steering + "," + throttle;
+        send(sending);
+    }
+
+    protected int cal_steering(float x, float y){
+        //若Y为负数则返回最大角度
+        /*if(y < 0){
+            if(x > 0){
+                return 90;
+            }else{
+                return 0;
+            }
+        }
+        */
+
+        //再计算值
+        x = x / gravity * 50;
+        int steering = (Math.round(x / 5) * 5) + 45;
+        //这个算法有点问题，角度大的时候会返回-5和95,，所以把这个去掉
+        if(steering < 0 ){
+            return 0;
+        }else if(steering > 90){
+            return 90;
+            //如果倾斜太少则视为没给油
+        }else if(steering > 35 && steering < 55){
+            return 45;
+        }else{
+            return steering;
+        }
+    }
+
+    protected int cal_throttle(float x,float y){
+        //若Y为负数则返回最大角度
+        if(y < 0){
+            if(x > 0){
+                return 100;
+            }else{
+                return -100;
+            }
+        }
+        //再计算值
+        x = x / gravity * 100;
+        x = (float)(Math.round(x/10)) * 10;
+
+        //如果倾斜太少则视为没给油
+        int intx = Math.round(x);
+        if(intx <=20 && intx >= -20){
+            return 0;
+        }else{
+            return intx;
+        }
+    }
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
